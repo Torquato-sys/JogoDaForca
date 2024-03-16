91b44ddebd13995b60dea7557127f54a033fbc30
Mateus Torquato Fernandes <mateustorquato98@gmail.com>
2024-03-15 23:09:49 -0300

    Boneco atualizado!

--- jogoDaForca.py
+++ jogoDaForca.py
@@ -41,9 +41,9 @@ while True:
         if erros == 2:
             linha2 = "  |  "
         elif erros == 3:
-            linha2 = " \|  "
+            linha2 = " /|  "
         elif erros >= 4:
-            linha2 = " \|/ "
+            linha2 = " /|\ "
         print(f"X{linha2}")
         linha3 = ""
         if erros == 5:
